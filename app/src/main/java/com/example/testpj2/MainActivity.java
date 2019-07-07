c9280eea2b33cd39a65a525df7dee7f88ef124c0
hansangjun <sang9163@gmail.com>
2019-07-07 10:42:46 +0900

    리소스 업데이트

--- app/src/main/java/com/example/testpj2/MainActivity.java
+++ app/src/main/java/com/example/testpj2/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "WELCOME MY FIRST PROJECT", Toast.LENGTH_SHORT).show();
-        //왜 추가가안되는겨ㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇ
+        //왜 추가가안되는겨ㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅊㅋㅊㅋㅌㅊㅋㅌㅊㅋㅌㅊㅋㅊㅋㅌㅊ
+
     }
 }
